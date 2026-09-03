333fbe069bc9e8f0027d70568ff0d30456bd499d
Mohammed Faizan <134358140+KinglerFaizan@users.noreply.github.com>
2026-09-04 00:05:58 +0530

    Fix import statement for API_KEY in main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 import streamlit as st
 
 try:
-    from config inport API_KEY as CONFIG_API_KEY
+    from config import API_KEY as CONFIG_API_KEY
 except ImportError:
     CONFIG_API_KEY = "1186ffb930c54389982ecd79cb9b5fae"
 
